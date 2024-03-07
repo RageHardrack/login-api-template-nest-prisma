c6001ab1e74dad52d613cdf9ed630d6b28bc80d5
Dan Colmenares <dacolmenares93@gmail.com>
2024-03-06 22:21:39 -0500

    Config Swagger and env.example

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Bootstrap');
+  app.setGlobalPrefix('api/');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('Login Template API')
+    .setDescription('Register/Login Template API with Prisma')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.enableCors();
+
+  // Prisma Global Filters
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  await app.listen(process.env.PORT);
+  logger.log(`App running on port: ${process.env.PORT}`);
 }
 bootstrap();
